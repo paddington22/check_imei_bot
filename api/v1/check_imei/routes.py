0d57165b539b3d2d705dcbc4c7b8c135115421bd
paddington22 <pkorolev23@gmail.com>
2025-01-24 11:14:16 +0300

    fix routes

--- api/v1/check_imei/routes.py
+++ api/v1/check_imei/routes.py
@@ -1,9 +1,12 @@
 from fastapi import APIRouter, HTTPException
-
+from .schemas import ImeiRequest
+from  utils.request import get_request
 
 router = APIRouter(prefix="/check-imei", tags=["Check-imei"])
 
 
 @router.post("")
-async def get_information():
-    pass
+async def get_information(info: ImeiRequest):
+    response = get_request(token=info.token, device_id=info.imei)
+    return response.json()
+
