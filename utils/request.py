9561a544541dd47e4987291f9def62243c4ae11c
paddington22 <pkorolev23@gmail.com>
2025-01-24 10:51:19 +0300

    fix request

--- utils/request.py
+++ utils/request.py
@@ -7,17 +7,13 @@ from settings.config import Settings
 # Base URL
 url = 'https://api.imeicheck.net/v1/checks'
 
-token = Settings.API_TOKEN
-
-# Add necessary headers
-headers = {
-    'Authorization': 'Bearer ' + token,
-    'Content-Type': 'application/json'
-}
-
 
 # Execute request
-def get_request(device_id):
+def get_request(token, device_id):
+    headers = {
+        'Authorization': 'Bearer ' + token,
+        'Content-Type': 'application/json'
+    }
     body = json.dumps({
         "deviceId": f"{device_id}",
         "serviceId": 1
