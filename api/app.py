862a4212d32a1f52b1e719375058e2823c08047b
paddington22 <pkorolev23@gmail.com>
2025-01-24 15:20:17 +0300

    add tortoise connection to lifespan

--- api/app.py
+++ api/app.py
@@ -1,6 +1,9 @@
 from contextlib import asynccontextmanager
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
+from tortoise import Tortoise
+
+from settings.config import TORTOISE_CONFIG
 
 from api.v1.check_imei.routes import router as check_imei_router
 from api.v1.auth.routes import router as auth_router
@@ -11,9 +14,11 @@ from utils.tg_bots import client_ptb
 @asynccontextmanager
 async def lifespan(_: FastAPI):
     webhook_base_url = settings.BOT_WEBHOOK_URL
+    await Tortoise.init(config=TORTOISE_CONFIG)
     await client_ptb.bot.set_webhook(f"{webhook_base_url}/telegram_webhooks/clients")
     yield
     await client_ptb.bot.delete_webhook()
+    await Tortoise.close_connections()
 
 app = FastAPI(lifespan=lifespan)
 
