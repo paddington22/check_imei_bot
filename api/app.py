6ec2c142a41bf885bf72e72f7ab976b879284bf2
paddington22 <pkorolev23@gmail.com>
2025-01-24 15:05:19 +0300

    add middleware

--- api/app.py
+++ api/app.py
@@ -1,8 +1,9 @@
 from contextlib import asynccontextmanager
-
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
 
 from api.v1.check_imei.routes import router as check_imei_router
+from api.v1.auth.routes import router as auth_router
 from settings.config import settings
 from utils.tg_bots import client_ptb
 
@@ -16,7 +17,13 @@ async def lifespan(_: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 
-
-app.include_router(
-    check_imei_router,
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
+
+app.include_router(check_imei_router)
+app.include_router(auth_router)
