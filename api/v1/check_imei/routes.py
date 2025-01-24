26c687a985197eb5f6ed6bd9d5f9304f2773a2fb
paddington22 <pkorolev23@gmail.com>
2025-01-24 15:14:23 +0300

    fix

--- api/v1/check_imei/routes.py
+++ api/v1/check_imei/routes.py
@@ -7,6 +7,6 @@ router = APIRouter(prefix="/check-imei", tags=["Check-imei"])
 
 @router.post("")
 async def get_information(payload: ImeiRequest):
-    response = get_request(token=info.token, device_id=info.imei)
+    response = get_request(token=payload.token, device_id=payload.imei)
     return response.json()
 
